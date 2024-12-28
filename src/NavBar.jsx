d2eb7809f8ed067cc9e2463484fc35f22d93813e
Alishbahanif1 <alishbave1@gmail.com>
2024-12-28 17:20:13 +0500

    Update NavBar.jsx

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -16,7 +16,7 @@ function NavBar() {
             
 
                
-                    {userId != 2 && userId !=3 &&(
+                    {userId ==3 &&(
                 <>
                   <li className="nav-item">
                     <Link className="nav-link" to="/allCustomers">View All Customers</Link>
